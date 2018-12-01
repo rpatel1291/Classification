c05eb4855da7fd31688a13f55be84ea801caf312
Ravi <Rpatel1291@gmail.com>
2018-12-01 13:54:30 -0500

    KKNN working

--- knn.r
+++ knn.r
@@ -6,11 +6,24 @@
 ####    setwd("~/Stevens/Fall2018/CS513_Knowledge_Discovery/Final Project/Classification")
 ####
 ##################################################################################################################
+setwd("~/Stevens/Fall2018/CS513_Knowledge_Discovery/Final Project/Classification")
 rm(list = ls())
-
 library(kknn)
 
 df_train <- read.csv("./data/train_new.csv")
 df_test <- read.csv("./data/test_new.csv")
 
-df_kknn <- kknn(OutcomeType~., train = df_train, test = df_test, k = 10)
+df_train$ID <- as.integer(df_train$ID)
+df_train$OutcomeType <- as.integer(df_train$OutcomeType)
+df_train$AnimalType <- as.integer(df_train$AnimalType)
+df_train$SimpleColor <- as.integer(df_train$SimpleColor)
+
+df_test$AnimalType <- as.integer(df_test$AnimalType)
+df_test$SimpleColor <- as.integer(df_test$SimpleColor)
+
+train_temp <- df_train[1:1000,]
+test_temp <- df_test[1:50,]
+
+df_kknn <- kknn(OutcomeType~., train = train_temp, test = test_temp, k = 10)
+fit<- fitted(df_kknn)
+table(df_test$OutcomeType, fit)
