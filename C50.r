f2a1f25b399fa72e803c149424df71ced034af87
Derek Pulaski <dpulaski@stevens.edu>
2018-12-01 16:50:15 -0500

    C50

---
+++ C50.r
@@ -0,0 +1,22 @@
+rm(list = ls())
+
+library('C50')
+
+df_train <- read.csv("./data/train_new.csv")
+df_test <- read.csv("./data/test_new.csv")
+
+df_train$ID <- as.integer(df_train$ID)
+
+mytree <- C5.0( OutcomeType~AnimalType + AgeinDays + HasName + IsNeutered + IsMix + Gender, data =df_train )
+summary(mytree)
+plot(mytree)
+
+library("partykit")
+myTree2 <- C50:::as.party.C5.0(mytree)
+plot(myTree2[2])
+plot(myTree2[10])
+
+prediction<-predict( mytree ,df_test , type="class" )
+table(actual=df_test[,3],prediction)
+wrong<- (df_test[,3]!=prediction)
+rate<-sum(wrong)/length(wrong)
