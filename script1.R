f07061c29fa01b09d7587a4308183d25b20d59ef
Ravi <Rpatel1291@gmail.com>
2018-12-01 12:08:27 -0500

    Update script1.R
    Created binary variable Has Name
    Remove blanks: SexuponOutcome, AgeuponOutcome
    Convert Age to Days

--- script1.R
+++ script1.R
@@ -28,16 +28,21 @@ library(randomForest)
 library(neuralnet)
 
 #### 1a. Cleaning of data ####
+
+#### Renaming of columns ####
 names(df_train)[1] <- 'ID'
 df_test$ID <- as.character(df_test$ID)
 
 full_df <- bind_rows(df_train, df_test)
 
-### Removing Unknown in SexuponOutcome ###
+#### Has name ####
+full_df$HasName <- ifelse(full_df$Name == "", 0, 1)
+
+### Removing Blanks in SexuponOutcome ###
 i <- 1
 temp <- c()
 for(x in full_df$SexuponOutcome){
-  if(x == "Unknown"){
+  if(x == ""){
     temp <- c(temp,i)
   }
   i <- i+1
@@ -47,7 +52,7 @@ rm(i)
 rm(temp)
 rm(x)
 
-### Removing blanks from AgeuponOutcome ###
+### Removing blanks from AgeuponOutcome ####
 i<-1
 temp <- c()
 for(x in full_df$AgeuponOutcome){
@@ -60,6 +65,18 @@ full_df<- full_df[-temp,]
 rm(i)
 rm(temp)
 rm(x)
+
+#### Converting to Age to Days ####
+full_df$TimeValue <- as.numeric(sapply(full_df$AgeuponOutcome, function(x) strsplit(x, split= ' ')[[1]][1]))
+full_df$TimeUnit <- sapply(full_df$AgeuponOutcome, function(x) strsplit(x, split= ' ')[[1]][2])
+full_df$TimeUnit <- gsub('s', '',full_df$TimeUnit)
+
+mult_vector <- ifelse(full_df$TimeUnit == 'day', 1,
+               ifelse(full_df$TimeUnit == 'week',7,
+               ifelse(full_df$TimeUnit == 'month',30,
+               ifelse(full_df$TimeUnit == 'year', 365, NA))))
+full_df$AgeinDays <- full_df$TimeValue * mult_vector
+
 
 #### 1b. Data Splits ####
 i<-1
@@ -82,3 +99,4 @@ summary(full_df)
 summary(df_train_new)
 summary(df_test_new)
 
+table(df_test_new$OutcomeType, df_test_new$AnimalType)
