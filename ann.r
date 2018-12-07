5f357ba11a98bfe23a6f342a4b5636c8aee86b5e
Ravi <Rpatel1291@gmail.com>
2018-12-07 11:00:49 -0500

    ANN able to run
    able to run  ann with hidden of 20,10,5 but with limited number of inputs.
    Unable to do error rate

--- ann.r
+++ ann.r
@@ -7,31 +7,44 @@
 ####
 ##################################################################################################################
 setwd("~/Stevens/Fall2018/CS513_Knowledge_Discovery/Final Project/Classification")
+
+#### 0. Cleaning Environment ####
+
 rm(list = ls())
 
+
+#### 1. Importing Data and Libraries ####
 library(neuralnet)
 
 df_train <- read.csv("./data/train_new.csv")
 df_test <- read.csv("./data/test_new.csv")
 
-df_train <- df_train[,2:10]
-df_test <- df_test[,2:10]
+#### 2. Removing Column ####
+df_train <- df_train[,-1]
+df_test <- df_test[,-1]
 
-df_train$ID <- as.integer(df_train$ID)
+#### 3. Neural Net ####
+
 df_train$OutcomeType <- as.integer(df_train$OutcomeType)
 df_train$AnimalType <- as.integer(df_train$AnimalType)
 df_train$SimpleColor <- as.integer(df_train$SimpleColor)
 df_train$Gender <- as.integer(df_train$Gender)
 df_train$AgeinDays <- as.integer(df_train$AgeinDays)
+df_train$HasName <- as.integer(df_train$HasName)
+df_train$IsNeutered <- as.integer(df_train$IsNeutered)
+df_train$IsMix <- as.integer(df_train$IsMix)
 
-df_test$AnimalType <- as.integer(df_test$AnimalType)
-df_test$SimpleColor <- as.integer(df_test$SimpleColor)
 
-df_ann <- neuralnet(OutcomeType ~ AnimalType + AgeinDays + HasName + IsNeutered + IsMix + Gender+ SimpleColor, data = df_train, threshold = 0.001, hidden = 10, linear.output = TRUE, stepmax = 100,rep = 3)
+df_ann <- neuralnet(OutcomeType~ AnimalType + AgeinDays + HasName + IsNeutered, data = df_train,linear.output = T, hidden=c(10,4,2))
+
+# + HasName+ IsNeutered + IsMix + Gender
+
+plot(df_ann)
+df_ann <- neuralnet(OutcomeType~ AnimalType + AgeinDays + HasName + IsNeutered + IsMix + Gender, data = df_train, threshold = 0.001, hidden = 10, stepmax = 10000)
 
 plot(df_ann)
 
-net_result <- compute(df_ann, df_test[.c(-1)])
+net_result <- compute(df_ann, df_test[,c(-1)])
 ann <- as.numeric(net_result$net.result)
 
 table(Actual=df_test$OutcomeType)
