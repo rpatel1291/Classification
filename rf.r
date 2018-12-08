993d641f4a4e99cc95bcc2e62e773e875a80b320
Derek Pulaski <dpulaski@stevens.edu>
2018-12-08 14:55:57 -0500

    added randomforest

---
+++ rf.r
@@ -0,0 +1,35 @@
+##################################################################################################################
+####  Purpose: Final Project - Classification - RandomForest
+####  Group: Ravi P., Derek P., Aneesh K., Ninad K.
+####  Date: 11/28/2018
+####  Comment:
+####    setwd("~/Stevens/Fall2018/CS513_Knowledge_Discovery/Final Project/Classification")
+####
+##################################################################################################################
+setwd("~/Stevens/Fall2018/CS513_Knowledge_Discovery/Final Project/Classification")
+rm(list = ls())
+
+df_train <- read.csv("./data/train_new.csv")
+df_test <- read.csv("./data/test_new.csv")
+
+df_train <- df_train[,2:10]
+df_test <- df_test[,2:10]
+
+levels(df_test$SimpleColor) <- levels(df_train$SimpleColor)
+
+
+# install.packages("randomForest")
+library(randomForest)
+
+
+fit <- randomForest( OutcomeType~., data=df_train[,-1], importance=TRUE, ntree=1000)
+importance(fit)
+varImpPlot(fit)
+Prediction <- predict(fit, df_test[,-1])
+table(actual=df_test$OutcomeType ,Prediction)
+
+
+wrong<- (df_test$OutcomeType!=Prediction )
+error_rate<-sum(wrong)/length(wrong)
+error_rate 
+
