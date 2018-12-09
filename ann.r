2236bf7495140462ccc98a4a86fbc62ef8402281
Ravi <Rpatel1291@gmail.com>
2018-12-09 12:26:20 -0500

    Update ann.r

--- ann.r
+++ ann.r
@@ -36,6 +36,15 @@ df_train$HasName <- as.integer(df_train$HasName)
 df_train$IsNeutered <- as.integer(df_train$IsNeutered)
 df_train$IsMix <- as.integer(df_train$IsMix)
 
+df_test$OutcomeType <- as.integer(df_test$OutcomeType)
+df_test$AnimalType <- as.integer(df_test$AnimalType)
+df_test$SimpleColor <- as.integer(df_test$SimpleColor)
+df_test$Gender <- as.integer(df_test$Gender)
+df_test$AgeinDays <- as.integer(df_test$AgeinDays)
+df_test$HasName <- as.integer(df_test$HasName)
+df_test$IsNeutered <- as.integer(df_test$IsNeutered)
+df_test$IsMix <- as.integer(df_test$IsMix)
+
 
 df_ann <- neuralnet(OutcomeType~ AnimalType + AgeinDays + HasName + IsNeutered + IsMix, data = df_train,linear.output = T, hidden=c(30,15,5))
 
@@ -45,7 +54,17 @@ df_ann <- neuralnet(OutcomeType~ AnimalType + AgeinDays + HasName + IsNeutered +
 plot(df_ann)
 summary(df_ann)
 
-net_result <- compute(df_ann, df_test)
+net_result <- compute(df_ann, df_test[,c("OutcomeType", "AnimalType", "AgeinDays", "HasName","IsNeutered", "IsMix")])
 ann <- as.numeric(net_result$net.result)
+ANN_cat<-ifelse(ann<1.5,1,
+        ifelse(ann<2.5,2,
+        ifelse(ann<3.5,3,
+        ifelse(ann<4.5,4,
+        ifelse(ann<5.5,5,6)))))
 
-table(Actual=df_test$OutcomeType)
+table(df_test$OutcomeType, ANN_cat)
+
+wrong <- (df_test$OutcomeType != ANN_cat)
+wrong
+rate<-sum(wrong)/length(wrong)
+rate
